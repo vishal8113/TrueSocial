5f4843ad8886d6c1bf89f0ff73f019c083a6df6b
Vishal Chauhan <vishal@kali.net>
2025-01-19 21:59:23 +0530

    Fix some bugs

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -1,34 +1,40 @@
-const User= require("../Models/authModel");
-const bcrypt= require("bcrypt");
+const User = require("../Models/authModel");
+const bcrypt = require("bcrypt");
 // User Registration Controller
-exports.register = async (req,res,next) => {
-    try{
-        const {email,password}=req.body;
-        const hash = await bcrypt.hash(password,12);
-        const user = await User.create({email,password:hash});
-        if(!user)
-        {
-         return res.status(503).json({
-            status:"error",
-           message:"Registration Failed",
-          })
+exports.register = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        if (password.length < 8) {
+            return res.status(403).json({
+                status: "error",
+                message: "Password must be at least 8 characters long"
+            })
+        } else {
+            const hash = await bcrypt.hash(password, 12);
+            const user = await User.create({ email, password: hash });
+            if (!user) {
+                return res.status(503).json({
+                    status: "error",
+                    message: "Registration Failed",
+                })
+            }
+            return res.status(200).json({
+                status: "success",
+                message: "Registration Successful"
+            })
         }
-        return res.status(200).json({
-            status:"success",
-            message:"Registration Successful"
-        })
+
 
     }
-    catch(err)
-    {
+    catch (err) {
         console.log(err);
         res.status(502).json({
-            message:err
+            message: err
         })
     }
 }
 
 // User Login Controller
-exports.login = async (req,res,next) => {
+exports.login = async (req, res, next) => {
     // Write Code..
 }
